2cf445d5f72bfe017c1ed1c2c57307ad99e470ce
meddokss <rvolyanskiy@gmail.com>
2016-10-28 19:24:16 +0300

    2

--- src/create.js
+++ src/create.js
@@ -1,7 +1,56 @@
-export class CreatePage {
 
-}
 
 export default function initPage() {
-	alert('create page')
+
+ 		
+     document.querySelector('.form').addEventListener('submit', (event) => {
+
+     	event.preventDefault();
+
+     	class User{
+            Constructor(name,password,email,comment){
+               this.name = name;
+               this.password = password;
+               this.email = email;
+               this.comment = comment;
+           }
+        }
+
+        var newUser = new User( 
+	          document.querySelector('.name').value,
+	          document.querySelector('.password').value,
+	          document.querySelector('.email').value,
+	          document.querySelector('.comment').value
+	    );
+
+
+		var myHeaders = new myHeaders();
+   		myHeaders.append("Content-Type", "application/json"); 
+
+	    var myInit = {
+						method: 'POST', 
+						headers: myHeaders,  
+						mode: 'cors',   
+						cache: 'default', 
+						body: JSON.stringify({
+							name:     newUser.name,
+							password: newUser.password,
+							email:    newUser.email,
+							comment:  newUser.comment
+	                	})               
+	    };
+
+    	var myRequest = new Request('http://193.111.63.76:3000/api/v1/Users', myInit); 
+		    fetch(myRequest) 
+			    .then(function(response) {
+			          return response.json(); 
+			    })
+    		.then(function(json) {
+                alert(JSON.stringify(json));
+  			});
+		});
 }
+
+
+
+
--- src/list.js
+++ src/list.js
@@ -3,5 +3,21 @@ export class ListPage {
 }
 
 export default function initPage() {
-	alert('list page')
+	var table = document.createElement('table');
+
+	fetch('http://193.111.63.76:3000/api/v1/Users')
+		.then(function(response) {
+		  return response.json();
+	})
+			.then(function(data) {
+			  // console.log(data);
+			  for (var i = 0; i < data.length; i++) {
+			    var row = table.insertRow(-1);
+			    for (var key in data[i]) {
+			      var cell = row.insertCell(-1);
+			      cell.innerHTML = data[i][key]; 
+			    }
+				    document.body.appendChild(table);
+				}
+			});
 }
